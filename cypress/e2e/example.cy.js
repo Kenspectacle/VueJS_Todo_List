c1ba3b09c00fc10a67f2ccf36254517e0d89e58d
Kenspectacle <Kennyftw2@gmail.com>
2024-02-18 21:42:20 +0100

    checks whether To-do List exist

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -4,4 +4,8 @@ describe('Test the Todo List', () => {
   it('Check whether the app is up ', () => {
     cy.visit('http://localhost:5173/')
   })
+  it('Check if the to-do list exist', () => {
+    cy.visit('http://localhost:5173/')
+    cy.contains('To-do List')
+  })
 })
