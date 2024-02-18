ad944fff1f4a284e257fb3ef631638b8fe33d33c
Kenspectacle <Kennyftw2@gmail.com>
2024-02-18 21:41:09 +0100

    linked all of the footer components

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,8 +1,11 @@
 <script setup>
+import TodoAddButton from './buttons/TodoAddButton.vue';
 import TodoClearButton from './buttons/TodoClearButton.vue'
 import TodoStatistics from './TodoStatistics.vue'
 </script>
 
 <template>
-    
+    <TodoAddButton />
+    <TodoClearButton />
+    <TodoStatistics />
 </template>
