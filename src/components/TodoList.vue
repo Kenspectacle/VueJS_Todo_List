9addfdf172d7c576789662330f2dab713163cbba
Kenspectacle <Kennyftw2@gmail.com>
2024-02-19 19:50:05 +0100

    feat: added new task and clear for todolist

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,14 +2,37 @@
 import TodoItem from './TodoItem.vue'
 import { ref } from 'vue'
 
-const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+const items = ref([
+    { id: 1, message: 'Foo', detail: 'lorem ipsum' },
+    { id: 2, message: 'Bar', detail: 'lorem ipsum' }])
+
+const newTask = ref('');
+
+function submitTask() {
+    if (newTask.value.trim() !== '') {
+        let newItem = {
+            id: 3,
+            message: newTask.value.trim(),
+            detail: 'lorem ipsum'
+        }
+        items.value.push(newItem);
+        newTask.value = ''; // Clear the input field after submitting
+    }
+}
+
+function clearTasks() {
+    items.value = []
+}
 </script>
 
 
 
 <template>
     <h2>The Todo List: </h2>
-    <li v-for="item in items">
-        {{ item.message }}
-    </li>
+    <ul>
+        <TodoItem v-for="item in items" :msg="item.message" :detail="item.detail" />
+    </ul>
+    <input type="text" placeholder="Enter task " v-model="newTask" />
+    <button @click="submitTask">Submit</button>
+    <button @click="clearTasks">Clear All</button>
 </template>
