4d5cb9810a2ae3c6e6be5fdaeedf9d36e3361daf
Kenspectacle <Kennyftw2@gmail.com>
2024-02-19 20:30:49 +0100

    added visibility toggle for the details

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -15,14 +15,18 @@ defineProps({
 })
 
 const isShowingDetail = ref(false)
+
+function detailToggle() {
+    isShowingDetail.value = !isShowingDetail.value
+}
 </script>
 
 <template>
-    <li>{{ msg }}
-        <TodoEditButton />
+    <li><button @click="detailToggle">{{ msg }}</button>
+        
         <TodoDeleteButton />
         <ul>
-            <li v-if="isShowingDetail"> {{ detail }}</li>
+            <li v-if="isShowingDetail"> {{ detail }} <TodoEditButton /> </li>
         </ul>
     </li>
 </template>
