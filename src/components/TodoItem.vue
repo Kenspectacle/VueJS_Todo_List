612c0b182b7681d3b259d22413ab8cc1be80bec0
Kenspectacle <Kennyftw2@gmail.com>
2024-02-19 20:20:06 +0100

    feat: made todo items visible

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,8 +1,26 @@
 <script setup>
 import TodoDeleteButton from './buttons/TodoDeleteButton.vue'
 import TodoEditButton from './buttons/TodoEditButton.vue'
+
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    },
+    detail: {
+        type: String,
+        required: true
+    }
+})
+
 </script>
 
 <template>
-
+    <li>{{ msg }}
+        <TodoEditButton />
+        <TodoDeleteButton />
+        <ul>
+            <li> {{ detail }}</li>
+        </ul>
+    </li>
 </template>
